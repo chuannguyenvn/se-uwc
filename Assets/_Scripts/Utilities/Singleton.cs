d180ece76e97ab8efb97b23e05132640a6d7c2de
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-14 19:38:37 +0700

    first commit!

---
+++ Assets/_Scripts/Utilities/Singleton.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance;
+
+    protected virtual void Awake() => Instance = this as T;
+
+    protected void OnApplicationQuit()
+    {
+        Instance = null;
+        Destroy(gameObject);
+    }
+
+    public static bool IsActive => Instance != null;
+}
+
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (Instance != null) Destroy(gameObject);
+        base.Awake();
+    }
+}
+
+public abstract class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+#if !UNITY_EDITOR
+        DontDestroyOnLoad(gameObject);
+#endif
+        base.Awake();
+    }
+}
