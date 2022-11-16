cee213f36aef13f4ec42534ab86ab87b518c5501
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-17 00:57:25 +0700

    feat: mock data generator

---
+++ Code/Assets/_Scripts/Database/MockDataGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MockDataGenerator : PersistentSingleton<MockDataGenerator>
+{
+    public DateTime GetRandomDateTime()
+    {
+        return DateTime.Today.AddDays(-Random.Range(100, 1000));
+    }
+
+    public StaffData GetMockStaffData()
+    {
+        return new StaffData(Random.Range(0, 1000).ToString(), "McPlaceholder", "photo", Gender.Male,
+            GetRandomDateTime(), "address", Random.Range(1000000000, 999999999).ToString(), "Vietnam",
+            GetRandomDateTime(), Random.Range(0, 2) == 0 ? Role.Collector : Role.Janitor,
+            Random.Range(1000000, 10000000));
+    }
+}
