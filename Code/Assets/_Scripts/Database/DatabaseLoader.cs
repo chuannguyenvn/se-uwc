54dcad504f3c11e02e16f65ec65aaf4483f1b07b
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-17 00:58:06 +0700

    feat: seperated database loader class

---
+++ Code/Assets/_Scripts/Database/DatabaseLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DatabaseLoader : PersistentSingleton<DatabaseLoader>
+{
+    public List<StaffData> LoadAllStaffData()
+    {
+        return new List<StaffData>()
+        {
+            MockDataGenerator.Instance.GetMockStaffData(),
+            MockDataGenerator.Instance.GetMockStaffData(),
+            MockDataGenerator.Instance.GetMockStaffData(),
+            MockDataGenerator.Instance.GetMockStaffData(),
+            MockDataGenerator.Instance.GetMockStaffData(),
+        };
+    }
+
+    public List<MCPData> LoadAllMCPData()
+    {
+        return new List<MCPData>() { };
+    }
+
+    public List<VehicleData> LoadAllVehicleData()
+    {
+        return new List<VehicleData>() { };
+    }
+
+    public List<MessageData> LoadAllMessageData()
+    {
+        return new List<MessageData>() { };
+    }
+
+    public List<TaskData> LoadAllTaskData()
+    {
+        return new List<TaskData>() { };
+    }
+
+    public List<MaintenanceLogData> LoadAllMaintenanceLogData()
+    {
+        return new List<MaintenanceLogData>() { };
+    }
+}
