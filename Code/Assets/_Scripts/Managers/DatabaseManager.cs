6f0c489080453790ee141866e584d3dc38caae81
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-17 00:58:27 +0700

    feat: database manager

---
+++ Code/Assets/_Scripts/Managers/DatabaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DatabaseManager : PersistentSingleton<DatabaseManager>
+{
+    private List<StaffData> allStaffs;
+    private List<MCPData> allMCPs;
+    private List<VehicleData> allVehicles;
+    private List<MessageData> allMessages;
+    private List<TaskData> allTasks;
+    private List<MaintenanceLogData> allMaintenanceLogs;
+
+    private void Start()
+    {
+        allStaffs = DatabaseLoader.Instance.LoadAllStaffData();
+        allMCPs = DatabaseLoader.Instance.LoadAllMCPData();
+        allVehicles = DatabaseLoader.Instance.LoadAllVehicleData();
+        allMessages = DatabaseLoader.Instance.LoadAllMessageData();
+        allTasks = DatabaseLoader.Instance.LoadAllTaskData();
+        allMaintenanceLogs = DatabaseLoader.Instance.LoadAllMaintenanceLogData();
+
+        foreach (var data in allStaffs)
+        {
+            Debug.Log(data);
+        }
+    }
+}
