7a34658f0ac146a52516e17a6e71ecd38604b07b
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-22 13:22:56 +0700

    feat: request routing json from Mapbox

--- Code/Assets/MapWrapper.cs
+++ Code/Assets/MapWrapper.cs
@@ -6,13 +6,15 @@ using Mapbox.Unity.Utilities;
 using Mapbox.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Networking;
 
 public class MapWrapper : MonoBehaviour
 {
     [SerializeField] private AbstractMap abstractMap;
 
     private Vector2 prevMousePos;
-
+    private Vector2d start;
+    private bool firstPointChosen = false;
 
     private void OnMouseDown()
     {
@@ -38,5 +40,41 @@ public class MapWrapper : MonoBehaviour
     {
         var newZoomValue = Mathf.Clamp(abstractMap.Zoom + Input.mouseScrollDelta.y / 5, 10, 20);
         abstractMap.UpdateMap(newZoomValue);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (firstPointChosen)
+            {
+                firstPointChosen = false;
+
+                var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                var end = abstractMap.WorldToGeoPosition(mousePos);
+
+                string uri = BuildRequestURI(start, end);
+                var request = UnityWebRequest.Get(uri);
+                StartCoroutine(ReadRequestResult(request));
+            }
+            else
+            {
+                firstPointChosen = true;
+                var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                start = abstractMap.WorldToGeoPosition(mousePos);
+            }
+        }
+    }
+
+    private string BuildRequestURI(Vector2d start, Vector2d end)
+    {
+        string head = "https://api.mapbox.com/directions/v5/mapbox/driving/";
+        string tail =
+            "?geometries=geojson&access_token=pk.eyJ1IjoiY2h1YW5wcm8wMzAiLCJhIjoiY2xhcG51ZWg5MDFqbTNwb2FlaW52MXNvciJ9.kFN5GOg3C8cGlk2PN4Tleg";
+
+        return head + start + ";" + end + tail;
+    }
+
+    private IEnumerator ReadRequestResult(UnityWebRequest request)
+    {
+        yield return request.SendWebRequest();
+        Debug.Log(request.downloadHandler.text);
     }
 }
