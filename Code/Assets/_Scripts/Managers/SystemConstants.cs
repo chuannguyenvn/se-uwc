be93ff955974c0bb5c5155c95256840c86cac8db
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-24 18:41:44 +0700

    feat: system-wide policy constants

---
+++ Code/Assets/_Scripts/Managers/SystemConstants.cs
@@ -0,0 +1,8 @@
+﻿public static class SystemConstants
+{
+    public static class MCP
+    {
+        public static float AlmostFullThreshold = 0.9f;
+        public static float FullyLoadedThreshold = 1f;
+    }
+}
