ca7a2f1489555b25ffa1d49b33d96ef38161da2f
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-11-30 23:43:15 +0700

    feat: list items are now clickable

--- Code/Assets/_Scripts/UI/List Item Views/DataListItemView.cs
+++ Code/Assets/_Scripts/UI/List Item Views/DataListItemView.cs
@@ -8,7 +8,7 @@ public abstract class DataListItemView<T> : ListItemView where T : Data
     [SerializeField] protected Image image;
     [SerializeField] protected TMP_Text primaryText_TMP;
     [SerializeField] protected TMP_Text secondaryText_TMP;
-
+    [SerializeField] protected Button button;
 
     public string PrimaryText;
     public string SecondaryText;
