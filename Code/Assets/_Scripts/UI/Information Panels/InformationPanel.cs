3b920776a84e107d026ccc715dafe4003f809150
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-02 10:06:10 +0700

    feat: hide information panels when changing view group

--- Code/Assets/_Scripts/UI/Information Panels/InformationPanel.cs
+++ Code/Assets/_Scripts/UI/Information Panels/InformationPanel.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public abstract class InformationPanel<T> : Singleton<InformationPanel<T>> where T : Data
 {
     [SerializeField] protected Button backButton;
+
+    protected virtual void Start()
+    {
+        PrimarySidebar.Instance.ViewChanged += ViewChangedHandler;
+        Hide();
+    }
 
     public virtual void Show(T data)
     {
@@ -13,5 +20,15 @@ public abstract class InformationPanel<T> : Singleton<InformationPanel<T>> where
     public virtual void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private void ViewChangedHandler(ViewType viewType)
+    {
+        Hide();
+    }
+
+    private void OnDestroy()
+    {
+        if (PrimarySidebar.Instance != null) PrimarySidebar.Instance.ViewChanged -= ViewChangedHandler;
     }
 }
