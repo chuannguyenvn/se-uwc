8a2ab6a1f58ad13b0137baf1c8f2401de48cbff1
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-02 22:15:11 +0700

    feat: show recipient's details on message box header

--- Code/Assets/_Scripts/UI/List Views/MessageDataListView.cs
+++ Code/Assets/_Scripts/UI/List Views/MessageDataListView.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MessageDataListView : DataListView<MessageData>
 {
+    [SerializeField] private Image profilePicture;
+    [SerializeField] private TMP_Text accountName;
+    [SerializeField] private TMP_Text status;
+    
     protected override void Init()
     {
         VERTICAL_SPACING = 20f;
@@ -14,12 +20,14 @@ public class MessageDataListView : DataListView<MessageData>
     private void InboxChosenHandler(Inbox inbox)
     {
         RemoveAllItemViews();
-
         foreach (var messageData in inbox.Messages)
         {
             AddDataItem(messageData);
         }
 
+        // profilePicture.sprite = [something];
+        accountName.text = inbox.RecipientName;
+        // status.text = [something];
         scrollRect.content.anchoredPosition = new Vector2(0, scrollRect.content.sizeDelta.y - 600);
     }
 
