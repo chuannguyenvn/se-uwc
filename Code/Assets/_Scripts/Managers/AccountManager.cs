313acc09e2c42c09e3c3272e6030d1a4650f43f4
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-02 11:40:42 +0700

    feat: AccountManager for managing the account in charge of the current session

---
+++ Code/Assets/_Scripts/Managers/AccountManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class AccountManager : PersistentSingleton<AccountManager>
+{
+    public string AccountID { get; private set; }
+
+    // Debug //
+
+    [SerializeField] private string debugAccountID;
+
+    private void Start()
+    {
+        AccountID = debugAccountID;
+    }
+
+    // Debug //
+}
