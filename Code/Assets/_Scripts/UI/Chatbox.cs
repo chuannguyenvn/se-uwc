1866a102430066f6d384b80e9fdc2ad437c224a3
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-05 14:46:54 +0700

    feat: chatbox

---
+++ Code/Assets/_Scripts/UI/Chatbox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class Chatbox : MonoBehaviour, IShowHideAnimatable
+{
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public virtual Task AnimateShow()
+    {
+        return rectTransform.DOAnchorPosX(0, VisualManager.Instance.ListAndPanelTime)
+            .SetEase(Ease.OutCubic)
+            .AsyncWaitForCompletion();
+    }
+
+    public virtual Task AnimateHide()
+    {
+        return rectTransform.DOAnchorPosX(2000, VisualManager.Instance.ListAndPanelTime)
+            .SetEase(Ease.InCubic)
+            .AsyncWaitForCompletion();
+    }
+}
