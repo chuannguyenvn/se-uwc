327b0b1a6b02b22df295f86f4301e9f143e4f488
Chuan <chuannguyen.vn@hcmut.edu.vn>
2022-12-07 00:47:43 +0700

    feat: mesages

--- Code/Assets/_Scripts/Backend Communications/MessageDatabaseCommunicator.cs
+++ Code/Assets/_Scripts/Backend Communications/MessageDatabaseCommunicator.cs
@@ -1,6 +1,55 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using Mapbox.Json;
+using UnityEngine;
+using UnityEngine.Networking;
 
 public class MessageDatabaseCommunicator : MonoBehaviour
 {
-    
+    private const string ADD_MESSAGE_PATH = "/api/message/add";
+    private const string GET_MESSAGE_PATH = "/api/message";
+
+    public void AddMessage(MessageData messageData, Action<bool> callback)
+    {
+        StartCoroutine(AddMessage_CO(messageData, callback));
+    }
+
+    private IEnumerator AddMessage_CO(MessageData messageData, Action<bool> callback)
+    {
+        string messageDataJson = JsonConvert.SerializeObject(messageData);
+
+        var request = BackendCommunicator.CreatePostRequest(ADD_MESSAGE_PATH, messageDataJson);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            callback?.Invoke(false);
+            yield break;
+        }
+
+        callback?.Invoke(true);
+    }
+
+    public void GetMessage(string accountId, Action<bool, CollectorRouteTraversedData> callback)
+    {
+        StartCoroutine(GetMessage_CO(accountId, callback));
+    }
+
+    private IEnumerator GetMessage_CO(string accountId,
+        Action<bool, CollectorRouteTraversedData> callback)
+    {
+        var request = BackendCommunicator.CreateGetRequest(string.Format(GET_MESSAGE_PATH, accountId));
+        yield return request.SendWebRequest();
+
+        Debug.Log(request.downloadHandler.text);
+        if (request.result is not UnityWebRequest.Result.Success or UnityWebRequest.Result.ProtocolError)
+        {
+            callback?.Invoke(false, null);
+            yield break;
+        }
+
+        var routeTraversedData =
+            JsonConvert.DeserializeObject<CollectorRouteTraversedData>(request.downloadHandler.text);
+        callback?.Invoke(true, routeTraversedData);
+    }
 }
--- Code/Assets/_Scripts/Database/POCOs/MessageData.cs
+++ Code/Assets/_Scripts/Database/POCOs/MessageData.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 public class MessageData : Data
 {
-    public string SenderID { get; set; }
-    public string ReceiverID { get; set; }
-
-    public DateTime Timestamp { get; set; }
-    public string Content { get; set; }
+    [JsonProperty("sender_id")] public string SenderID { get; set; }
+    [JsonProperty("receiver_id")] public string ReceiverID { get; set; }
+    [JsonProperty("sentAt")] public DateTime Timestamp { get; set; }
+    [JsonProperty("content")] public string Content { get; set; }
 
     public MessageData(string id, string senderID, string receiverID, DateTime timestamp,
         string content) : base(id)
